950e02bbb3ee8de68b4cf2dea88317c9392897a7
jhonfernandezlopez21-debug <jhonfernandezlopez21@gmail.com>
2026-02-23 22:26:00 -0400

    Update FacturaServiceTests.java

--- backend/src/test/java/com/Paciente/Gestion_Paciente/Factura/FacturaServiceTests.java
+++ backend/src/test/java/com/Paciente/Gestion_Paciente/Factura/FacturaServiceTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
+//a
 import java.util.Optional;
 import java.math.BigDecimal;
 
@@ -110,4 +110,5 @@ public class FacturaServiceTests {
         assertEquals(savedP.getIdPaciente(), opt.get().getPaciente().getIdPaciente());
     }
 }
+
 
